40e4993243a5128774f1206cfa83219d61326b1e
Quasikyo <michael@michaelrehman.com>
2024-08-24 11:12:01 -0500

    Enforcement of personal style

--- Assets/UdonSharp/UtilityScripts/BoneFollower.cs
+++ Assets/UdonSharp/UtilityScripts/BoneFollower.cs
@@ -4,32 +4,28 @@ using VRC.SDK3.Components;
 using VRC.SDKBase;
 using VRC.Udon;
 
-namespace UdonSharp.Examples.Utilities
-{
-    /// <summary>
-    /// Follows a chosen bone on humanoid avatars using the playerapi
-    /// </summary>
-    [AddComponentMenu("Udon Sharp/Utilities/Bone Follower")]
-    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
-    public class BoneFollower : UdonSharpBehaviour 
-    {
-        public HumanBodyBones trackedBone;
+namespace UdonSharp.Examples.Utilities {
+	/// <summary>
+	/// Follows a chosen bone on humanoid avatars using the playerapi
+	/// </summary>
+	[AddComponentMenu("Udon Sharp/Utilities/Bone Follower")]
+	[UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+	public class BoneFollower : UdonSharpBehaviour {
+		public HumanBodyBones trackedBone;
 
-        VRCPlayerApi playerApi;
-        bool isInEditor;
+		VRCPlayerApi playerApi;
+		bool isInEditor;
 
-        void Start()
-        {
-            playerApi = Networking.LocalPlayer;
-            isInEditor = playerApi == null;
-        }
+		void Start() {
+			playerApi = Networking.LocalPlayer;
+			isInEditor = playerApi == null;
+		}
 
-        void Update()
-        {
-            if (isInEditor)
-                return;
+		void Update() {
+			if (isInEditor)
+				return;
 
-            transform.SetPositionAndRotation(playerApi.GetBonePosition(trackedBone), playerApi.GetBoneRotation(trackedBone));
-        }
-    }
+			transform.SetPositionAndRotation(playerApi.GetBonePosition(trackedBone), playerApi.GetBoneRotation(trackedBone));
+		}
+	}
 }
--- Assets/UdonSharp/UtilityScripts/PlayerModSetter.cs
+++ Assets/UdonSharp/UtilityScripts/PlayerModSetter.cs
@@ -3,39 +3,35 @@ using UnityEngine;
 using UdonSharp;
 using VRC.SDKBase;
 
-namespace UdonSharp.Examples.Utilities
-{
-    [AddComponentMenu("Udon Sharp/Utilities/Player Mod Setter")]
-    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
-    public class PlayerModSetter : UdonSharpBehaviour
-    {
-        public float jumpHeight = 3f;
-        public float runSpeed = 4f;
-        public float walkSpeed = 2f;
-        public float strafeSpeed = 2f;
-        public float gravity = 1f;
+namespace UdonSharp.Examples.Utilities {
+	[AddComponentMenu("Udon Sharp/Utilities/Player Mod Setter")]
+	[UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+	public class PlayerModSetter : UdonSharpBehaviour {
+		public float jumpHeight = 3f;
+		public float runSpeed = 4f;
+		public float walkSpeed = 2f;
+		public float strafeSpeed = 2f;
+		public float gravity = 1f;
 
-        [Tooltip("Enables legacy locomotion which allows stutter stepping and wall climbing")]
-        public bool useLegacyLocomotion = false;
+		[Tooltip("Enables legacy locomotion which allows stutter stepping and wall climbing")]
+		public bool useLegacyLocomotion = false;
 
-        void Start()
-        {
-            var playerApi = Networking.LocalPlayer;
+		void Start() {
+			var playerApi = Networking.LocalPlayer;
 
-            // Prevent error in editor from null player API
-            if (playerApi != null)
-            {
-                playerApi.SetJumpImpulse(jumpHeight);
-                playerApi.SetRunSpeed(runSpeed);
-                playerApi.SetWalkSpeed(walkSpeed);
-                playerApi.SetStrafeSpeed(strafeSpeed);
-                playerApi.SetGravityStrength(gravity);
+			// Prevent error in editor from null player API
+			if (playerApi != null) {
+				playerApi.SetJumpImpulse(jumpHeight);
+				playerApi.SetRunSpeed(runSpeed);
+				playerApi.SetWalkSpeed(walkSpeed);
+				playerApi.SetStrafeSpeed(strafeSpeed);
+				playerApi.SetGravityStrength(gravity);
 
-                if (useLegacyLocomotion)
-                    playerApi.UseLegacyLocomotion();
-            }
+				if (useLegacyLocomotion)
+					playerApi.UseLegacyLocomotion();
+			}
 
-            Destroy(this);
-        }
-    }
+			Destroy(this);
+		}
+	}
 }
